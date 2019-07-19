d4ace9a4c6b1bb81e281315c4a32632e33b1c9ab
Andrew Andrew <GoldenRus86@yandex.ru>
2019-07-20 01:15:38 +0300

    Добавьте файлы проекта.

---
+++ EmployeeApplic/Employee.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApplic
+{
+    class Employee
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+        private string empSsn;
+
+        //Свойства
+        //Способ 1
+        //public string GetName()
+        //{
+        //    return empName;
+        //}
+
+        //public void SetName(string name)
+        //{
+        //    if (name.Length >= 15)
+        //    {
+        //        Console.WriteLine("Ошибка! Длина имени должна быть не более 15 символов.");
+        //    }
+        //    else
+        //        empName = name;
+        //}
+        //Способ 2
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Ошибка! Длина имени должна быть не более 15 символов.");
+                else empName = value;
+            }
+        }
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
+        public string SocialSecurityNumber
+        {
+            get { return empSsn; }
+        }
+
+        //Конструкторы
+        public Employee() { }
+        public Employee(string name, int id)
+            : this(name, 0, id, 0, "") { }
+        public Employee(string name, int id, float pay)
+            : this(name, 0, id, pay, "") { }
+
+        public Employee(string name, int age, int id, float pay, string ssn)
+        {
+            Name = name;
+            ID = id;
+            Pay = pay;
+            Age = age;
+            empSsn = ssn;
+        }
+
+        //Методы
+        public void GiveBonus(float amount)
+        {
+            Pay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine($"Имя сотрудника: {Name}");
+            Console.WriteLine($"Номер индетификационной карты: {ID}");
+            Console.WriteLine($"Текущая выплата: {Pay}");
+
+            if (Age >= 1)
+            {
+                Console.WriteLine($"Возраст сотрудника: {Age}");
+            }
+
+            if (SocialSecurityNumber != "")
+            {
+                Console.WriteLine($"SSN: {SocialSecurityNumber}");
+            }
+        }
+    }
+}
---
+++ EmployeeApplic/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApplic
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Employee c1 = new Employee("Rico", 1324573587, 4500);
+            c1.DisplayStats();
+            Console.WriteLine("==============");
+
+            Employee c2 = new Employee("Rider", 154665, 4100);
+            c2.DisplayStats();
+            c2.Name = "River";
+            Console.WriteLine();
+            c2.GiveBonus(150);
+            c2.DisplayStats();
+            Console.WriteLine("=======================");
+
+            Employee c3 = new Employee("Mike", 33, 124785787, 25478.34F, "");
+            c3.DisplayStats();
+
+            Employee c4 = new Employee("", 0, 0)
+            {
+                Name = "Fedor",
+                ID = 475787888,
+                Pay = 4787,
+                Age = 45
+
+            };
+
+            c4.DisplayStats();
+
+            Employee c5 = new Employee("Borut", 47, 745472456, 4757.45F, "4784576544");
+            c5.DisplayStats();
+
+            Console.ReadKey();
+        }
+    }
+}
